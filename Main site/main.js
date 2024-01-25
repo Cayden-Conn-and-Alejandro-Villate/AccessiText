dae999c8fe5a2854b28384f09cff1ecc6d609bf6
Ale V <alejandro.the.villate@gmail.com>
2024-01-24 21:19:59 -0500

    Made reading window draggable

--- Main site/main.js
+++ Main site/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOMContentLoaded");
+  dragElement(document.getElementById("interface"))
 });
 
 const readingWindow = document.getElementById("reading-window");
@@ -55,4 +56,45 @@ const updateFontStyle = () => {
 const updateLineSpacing = () => {
   const newLineSpacing = parseFloat(lineSpacing.value) + 0.2
   readingWindow.style.lineHeight = newLineSpacing
+}
+
+//Drag
+dragElement(document.getElementById("interface"))
+
+//citation: this is straight from W3 schools (https://www.w3schools.com/howto/howto_js_draggable.asp)
+function dragElement(elmnt) {
+  console.log('dragging')
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  document.getElementById("drag-menu").onmousedown = dragMouseDown;
+  
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
